0736ae995fd81751753013da529ec49c20d65ea4
Vivien <vdebona@umanit.fr>
2021-04-17 17:57:18 +0200

    Add a controller to handle media collections
    - Re-arrange markup to declare stimulus controller on each media item

---
+++ src/Resources/assets/src/collection.js
@@ -0,0 +1,62 @@
+import { Controller } from 'stimulus';
+
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+  static targets = ['list', 'addBtn'];
+  static values = {
+    allowAdd: Boolean,
+    allowDelete: Boolean,
+    min: Number,
+    max: Number,
+    initialNbElements: Number,
+    prototype: String,
+    nbElements: Number,
+  };
+
+  initialize() {
+    for (let i = 0; i < this.initialNbElementsValue; i++) {
+      this.add();
+    }
+  }
+
+  add(e) {
+    if (e) {
+      e.preventDefault();
+    }
+
+    if ('' === this.prototypeValue) {
+      return;
+    }
+
+    const itemProto = this.prototypeValue
+      .replace(/__name__label__/g, this.nbElementsValue)
+      .replace(/__name__/g, this.nbElementsValue)
+    ;
+
+    this.listTarget.insertAdjacentHTML('beforeend', itemProto);
+
+    ++this.nbElementsValue;
+  }
+
+  remove(e) {
+    if (e) {
+      e.preventDefault();
+    }
+
+    const item = e.currentTarget.closest('[data-umanit--ux-block-collection-bundle--collection-target="item"]');
+
+    if (null !== item) {
+      item.remove();
+
+      --this.nbElementsValue;
+    }
+  }
+
+  nbElementsValueChanged() {
+    if (this.maxValue > this.nbElementsValue) {
+      this.addBtnTarget.removeAttribute('disabled');
+    } else {
+      this.addBtnTarget.setAttribute('disabled', true);
+    }
+  }
+}
